aed143539bab7d85e7a3405931826492696767fa
kaiserkarel <k.l.kubat@gmail.com>
2022-09-12 12:50:12 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nix-pandoc.url = "github:serokell/nix-pandoc";
+    nix-pandoc.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, nix-pandoc }: {
+    defaultPackage = builtins.mapAttrs (system: pkgs: nix-pandoc.mkDoc.${system} {
+      name = "whitepaper";
+      src = ./.;
+      phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+      buildPhase = "pandoc --toc --citeproc --pdf-engine=xelatex -o $name.pdf ./whitepaper.md";
+      installPhase = "mkdir -p $out; cp $name.pdf $out";
+    }) nixpkgs.legacyPackages;
+  };
+}
